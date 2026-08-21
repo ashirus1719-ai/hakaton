08d8584f7988237747a7a447c5a04634aa5651d4
anabek2044-svg <Anarbek2044@gmail.com>
2026-08-21 16:43:04 +0300

    NotFound

--- pizza/src/router.jsx
+++ pizza/src/router.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Panel from "./pages/Panel";
+import NotFound from "./pages/NotFound";
 
 
 const routes = [
